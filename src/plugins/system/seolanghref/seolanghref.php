e8789a148d95975aaba074e9de85fd8aa6cd956b
VillaesterModerneMedien <77295564+VillaesterModerneMedien@users.noreply.github.com>
2022-10-14 17:59:05 +0200

    Äderung Header Infos

--- src/plugins/system/seolanghref/seolanghref.php
+++ src/plugins/system/seolanghref/seolanghref.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * @package     Joomla.Plugin
- * @subpackage  System.remember
- *
- * @copyright   (C) 2007 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+* SEO Lang HREF
+*
+* @package     Joomla.Plugin
+* @subpackage  System.Seolanghref
+*
+* @author      Whykiki <cs@villaester.de>
+* @copyright   villaester.de - All rights reserved.
+* @license     GNU General Public License version 3 or later
+*/
 
 
 defined('_JEXEC') or die;
