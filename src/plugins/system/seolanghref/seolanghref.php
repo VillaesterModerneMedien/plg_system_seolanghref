97f5eb629f9d28a9ca30c7806ebb42d62396b9f4
VillaesterModerneMedien <77295564+VillaesterModerneMedien@users.noreply.github.com>
2022-11-07 12:44:30 +0100

    RSForm Preview Fix

--- src/plugins/system/seolanghref/seolanghref.php
+++ src/plugins/system/seolanghref/seolanghref.php
@@ -1,132 +1,132 @@
 <?php
-/**
-* SEO Lang HREF
-*
-* @package     Joomla.Plugin
-* @subpackage  System.Seolanghref
-*
-* @author      Whykiki <cs@villaester.de>
-* @copyright   villaester.de - All rights reserved.
-* @license     GNU General Public License version 3 or later
-*/
-
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Associations;
-use Joomla\CMS\Language\LanguageHelper;
-use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Router\Route;
-use Joomla\CMS\Router\Router;
-use Joomla\CMS\Uri\Uri;
-use Joomla\Component\Menus\Administrator\Helper\AssociationsHelper;
-use Joomla\Module\Menu\Site\Helper\MenuHelper;
-
-/**
- * Joomla! System Remember Me Plugin
- *
- * @since  1.5
- */
-
-class PlgSystemSeolanghref extends CMSPlugin
-{
-    /**
-     * Change Head Links
-     *
-     * @return  void
-     * @since   1.0.0
-	 */
-
-    public function onBeforeCompileHead()
-    {
-	    $doc = Factory::getDocument();
-
-	    $app  = Factory::getApplication();
-		
-		if($app->isClient('administrator'))
-		{
-			return;
-		}
-
-        $active = $app->getMenu()->getActive();
-	    $associations = Associations::getAssociations('com_menus', '#__menu', 'com_menus.item', $active->id);
-		
-		if(!empty($associations))
-		{
-		    $lang        = Factory::getLanguage();
-		    $currentLanguageTag = $lang->get('tag');
-		    $currentLanguageTagShort = substr(strtolower($lang->get('tag')), 0, 2);
-			
-		    $head_data = $doc->getHeadData();
-
-		    $links     = $head_data['links'];
-			
-			$allLanguages  = LanguageHelper::getLanguages('lang_code');
-
-			$baseUrl = Uri::base();
-
-			$currentUrl = Uri::current();
-
-	        foreach ($allLanguages as $langTag => $values)
-	        {
-				$langTagShort = substr($langTag, 0, 2);
-
-				if($langTag !== $currentLanguageTag)
-				{
-					$menuItemId = (int) $associations[$langTag]->id;
-			        $menu = $app->getMenu();
-					$menuItem = $menu->getItem( $menuItemId );
-
-					$link = Route::_($menuItem->link . '&Itemid=' . $menuItem->id . '&lang=' . $langTagShort);
-
-					unset($doc->_links[$baseUrl . substr($link, 1)]);
-					$doc->addHeadLink($baseUrl . substr($link, 1), 'alternate', 'rel',['hreflang' => $langTagShort]);
-				}
-				else{
-					$link = Route::_($active->link . '&Itemid=' . $active->id . '&lang=' . $currentLanguageTagShort);
-					unset($doc->_links[$baseUrl . substr($link, 1)]);
-					$doc->addHeadLink($baseUrl . substr($link, 1) . '####', 'alternate', 'rel',['hreflang' => $currentLanguageTagShort]);
-				}
-	        }
-		}
-		
-	    if(Factory::getLanguage()->get('tag') != '')
-	    {
-		    $lang        = Factory::getLanguage();
-		    $currentLanguageTagShort = substr(strtolower($lang->get('tag')), 0, 2);
-		    $currentUrl = Uri::current();
-			
-		    $doc->addHeadLink($currentUrl, 'canonical', 'rel',['hreflang' => $currentLanguageTagShort]);
-		    $doc->addHeadLink($currentUrl . '####', 'alternate', 'rel',['hreflang' => $currentLanguageTagShort]);
-	    }
-	    else{
-		    $currentUrl = Uri::current();
-		    $doc->addHeadLink($currentUrl, 'canonical', 'rel');
-		    $doc->addHeadLink($currentUrl, 'canonical', 'rel',['hreflang' => $currentLanguageTagShort]);
-		    $doc->addHeadLink($currentUrl . '####', 'alternate', 'rel');
-	    }
-    }
-
 	/**
-	 * Listener for the `onAfterRender` event
+	 * SEO Lang HREF
 	 *
-	 * @return  void
-	 * @throws  \Exception
+	 * @package     Joomla.Plugin
+	 * @subpackage  System.Seolanghref
 	 *
-	 * @since   1.0.0
+	 * @author      Whykiki <cs@villaester.de>
+	 * @copyright   villaester.de - All rights reserved.
+	 * @license     GNU General Public License version 3 or later
 	 */
-	public function onAfterRender()
+	
+	defined('_JEXEC') or die;
+	
+	use Joomla\CMS\Factory;
+	use Joomla\CMS\Language\Associations;
+	use Joomla\CMS\Language\LanguageHelper;
+	use Joomla\CMS\Plugin\CMSPlugin;
+	use Joomla\CMS\Router\Route;
+	use Joomla\CMS\Router\Router;
+	use Joomla\CMS\Uri\Uri;
+	use Joomla\Component\Menus\Administrator\Helper\AssociationsHelper;
+	use Joomla\Module\Menu\Site\Helper\MenuHelper;
+	
+	/**
+	 * Joomla! System Remember Me Plugin
+	 *
+	 * @since  1.5
+	 */
+	
+	class PlgSystemSeolanghref extends CMSPlugin
 	{
-
-		$app = Factory::getApplication();
-		if ($app->isClient('administrator'))
+		/**
+		 * Change Head Links
+		 *
+		 * @return  void
+		 * @since   1.0.0
+		 */
+		
+		public function onBeforeCompileHead()
 		{
-			return;
+			$doc = Factory::getDocument();
+			
+			$app  = Factory::getApplication();
+			
+			if($app->isClient('administrator') || $app->input->get('option') == 'com_rsform')
+			{
+				return;
+			}
+			
+			$active = $app->getMenu()->getActive();
+			$associations = Associations::getAssociations('com_menus', '#__menu', 'com_menus.item', $active->id);
+			
+			if(!empty($associations))
+			{
+				$lang        = Factory::getLanguage();
+				$currentLanguageTag = $lang->get('tag');
+				$currentLanguageTagShort = substr(strtolower($lang->get('tag')), 0, 2);
+				
+				$head_data = $doc->getHeadData();
+				
+				$links     = $head_data['links'];
+				
+				$allLanguages  = LanguageHelper::getLanguages('lang_code');
+				
+				$baseUrl = Uri::base();
+				
+				$currentUrl = Uri::current();
+				
+				foreach ($allLanguages as $langTag => $values)
+				{
+					$langTagShort = substr($langTag, 0, 2);
+					
+					if($langTag !== $currentLanguageTag)
+					{
+						$menuItemId = (int) $associations[$langTag]->id;
+						$menu = $app->getMenu();
+						$menuItem = $menu->getItem( $menuItemId );
+						
+						$link = Route::_($menuItem->link . '&Itemid=' . $menuItem->id . '&lang=' . $langTagShort);
+						
+						unset($doc->_links[$baseUrl . substr($link, 1)]);
+						$doc->addHeadLink($baseUrl . substr($link, 1), 'alternate', 'rel',['hreflang' => $langTagShort]);
+					}
+					else{
+						$link = Route::_($active->link . '&Itemid=' . $active->id . '&lang=' . $currentLanguageTagShort);
+						unset($doc->_links[$baseUrl . substr($link, 1)]);
+						$doc->addHeadLink($baseUrl . substr($link, 1) . '####', 'alternate', 'rel',['hreflang' => $currentLanguageTagShort]);
+					}
+				}
+			}
+			
+			if(Factory::getLanguage()->get('tag') != '')
+			{
+				$lang        = Factory::getLanguage();
+				$currentLanguageTagShort = substr(strtolower($lang->get('tag')), 0, 2);
+				$currentUrl = Uri::current();
+				
+				$doc->addHeadLink($currentUrl, 'canonical', 'rel',['hreflang' => $currentLanguageTagShort]);
+				$doc->addHeadLink($currentUrl . '####', 'alternate', 'rel',['hreflang' => $currentLanguageTagShort]);
+			}
+			else{
+				$currentUrl = Uri::current();
+				$doc->addHeadLink($currentUrl, 'canonical', 'rel');
+				$doc->addHeadLink($currentUrl, 'canonical', 'rel',['hreflang' => $currentLanguageTagShort]);
+				$doc->addHeadLink($currentUrl . '####', 'alternate', 'rel');
+			}
 		}
-
-		$body = $app->getBody();
-		$bodyNew = str_replace('####', '', $body);
-		$app->setBody($bodyNew);
+		
+		/**
+		 * Listener for the `onAfterRender` event
+		 *
+		 * @return  void
+		 * @throws  \Exception
+		 *
+		 * @since   1.0.0
+		 */
+		public function onAfterRender()
+		{
+			
+			$app = Factory::getApplication();
+			if ($app->isClient('administrator'))
+			{
+				return;
+			}
+			
+			$body = $app->getBody();
+			$bodyNew = str_replace('####', '', $body);
+			$app->setBody($bodyNew);
+		}
+		
 	}
-
-}
